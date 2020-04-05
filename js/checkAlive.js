254f095a94ed0f0cf532d42c74cf22aae8b2b4b7
Roy Wong <whh881114@gmail.com>
2020-04-05 21:37:05 +0800

    将api后端的地址和端口都改成和lb一致。

--- js/checkAlive.js
+++ js/checkAlive.js
@@ -1,12 +1,12 @@
 function checkAlive() {
-    var api_host = "http://api.lnmp.example.com:8888";
+    var api_host = "http://api.lamp.example.com:40002";
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("is_srv_alive").innerHTML = "The api.lnmp.example.com is alive.";
+            document.getElementById("is_srv_alive").innerHTML = "The api.lamp.example.com is alive.";
             document.getElementById("is_srv_alive").style.color = "blue";
         } else {
-            document.getElementById("is_srv_alive").innerHTML = "The api.lnmp.example.com is dead.";
+            document.getElementById("is_srv_alive").innerHTML = "The api.lamp.example.com is dead.";
             document.getElementById("is_srv_alive").style.color = "red";
         }
     };
